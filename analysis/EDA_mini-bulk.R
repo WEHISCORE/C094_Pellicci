8167dd9a4d15b1d50ba1973942953d11096ecaf2
PeteHaitch <peter.hickey@gmail.com>
2021-06-01 15:30:00 +1000

    Update EDA_mini-bulk.R, add Dan's marker proteins, and render

--- analysis/EDA_mini-bulk.R
+++ analysis/EDA_mini-bulk.R
@@ -2,16 +2,35 @@
 # Peter Hickey
 # 2021-04-16
 
-# Setup mini-bulk data ---------------------------------------------------------
+#' # Setup
+#' ## Load mini-bulk data
 
 library(SingleCellExperiment)
 library(here)
-sce <- readRDS("data/SCEs/C094_Pellicci.scPipe.SCE.rds")
+sce <- readRDS(here("data/SCEs/C094_Pellicci.scPipe.SCE.rds"))
 sce <- sce[, sce$sample_type != "Single cell"]
 sce <- sce[, sce$sample_name != "Cell line"]
 sce$sample_name <- sub(" P[0-9]$", "", sce$sample_name)
+sce$tissue <- sapply(strsplit(sce$sample_name, " "), "[[", 1)
+sce$replicate <- sapply(strsplit(sce$sample_name, " "), "[[", 2)
 
-# Gene annotations -------------------------------------------------------------
+#' ## Helper functions
+
+# Take a DataFrame with AtomicList columns and return a DataFrame where these
+# columns have been flattened by paste-ing together the elements separated by
+# `sep`.
+flattenDF <- function(x, sep = "; ") {
+  DataFrame(
+    endoapply(x, function(xx) {
+      if (!is(xx, "AtomicList")) {
+        return(xx)
+      }
+      unstrsplit(as(xx, "CharacterList"), sep = sep)
+    }),
+    row.names = rownames(x))
+}
+
+#' ## Add gene annotations
 
 # Extract rownames (Ensembl IDs) to use as key in database lookups.
 ensembl <- rownames(sce)
@@ -64,6 +83,7 @@ ncbi_df <- DataFrame(
 rowData(sce) <- cbind(ensdb_df, ncbi_df)
 
 # Replace the row names of the SCE by the gene symbols (where available).
+library(scater)
 rownames(sce) <- uniquifyFeatureNames(
   ID = rownames(sce),
   # NOTE: An Ensembl ID may map to 0, 1, 2, 3, ... gene symbols.
@@ -77,166 +97,135 @@ rownames(sce) <- uniquifyFeatureNames(
   },
   character(1)))
 
-# Some useful gene sets
-mito_set <- rownames(sce)[any(rowData(sce)$ENSEMBL.SEQNAME == "MT")]
-ribo_set <- grep("^RP(S|L)", rownames(sce), value = TRUE)
-# NOTE: A more curated approach for identifying ribosomal protein genes
-#       (https://github.com/Bioconductor/OrchestratingSingleCellAnalysis-base/blob/ae201bf26e3e4fa82d9165d8abf4f4dc4b8e5a68/feature-selection.Rmd#L376-L380)
-library(msigdbr)
-c2_sets <- msigdbr(species = "Homo sapiens", category = "C2")
-ribo_set <- union(
-  ribo_set,
-  c2_sets[c2_sets$gs_name == "KEGG_RIBOSOME", ]$gene_symbol)
-ribo_set <- intersect(ribo_set, rownames(sce))
-sex_set <- rownames(sce)[any(rowData(sce)$ENSEMBL.SEQNAME %in% c("X", "Y"))]
-pseudogene_set <- rownames(sce)[
-  any(grepl("pseudogene", rowData(sce)$ENSEMBL.GENEBIOTYPE))]
-
-# Helper functions -------------------------------------------------------------
-
-# Take a DataFrame with AtomicList columns and return a DataFrame where these
-# columns have been flattened by paste-ing together the elements separated by
-# `sep`.
-flattenDF <- function(x, sep = "; ") {
-  DataFrame(
-    endoapply(x, function(xx) {
-      if (!is(xx, "AtomicList")) {
-        return(xx)
-      }
-      unstrsplit(as(xx, "CharacterList"), sep = sep)
-    }),
-    row.names = rownames(x))
-}
-
-
-library(scater)
-createClusterMarkerOutputs <- function(sce, outdir, markers, k, ...) {
-
-  # Create CSVs
-  message("Creating CSVs")
-  for (label in names(markers)) {
-    message("\t", label)
-    gzout <- gzfile(
-      description = file.path(
-        outdir,
-        sprintf("%s.csv.gz", label)),
-      open = "wb")
-    write.csv(
-      as.data.frame(markers[[label]]),
-      gzout,
-      # NOTE: quote = TRUE needed because some fields contain commas.
-      quote = TRUE,
-      row.names = FALSE)
-    close(gzout)
-  }
-
-  # Create PDFs
-  message("Creating PDFs")
-  for (label in names(markers)) {
-    message("\t", label)
-    label_markers <- markers[[label]]
-    # Filter genes
-    # NOTE: No FDR cut-off since we're using the underpowered t-test
-    features <- rownames(label_markers)
-    # TODO: Useful?
-    features <- setdiff(features, c(ribo_set, mito_set))
-    # Select top-k
-    features <- head(features, k)
-    if (length(features) >= 2) {
-
-      plotHeatmap(
-        sce,
-        features,
-        color = hcl.colors(101, "Blue-Red 3"),
-        label_cols = TRUE,
-        show_colnames = FALSE,
-        order_columns_by = c("tissue", "sample_name", "sample_gate"),
-        center = TRUE,
-        symmetric = TRUE,
-        zlim = c(-3, 3),
-        fontsize = 6,
-        main = label,
-        # NOTE: Leave it to pheatmap() to create the PDFs because multipage
-        #       PDFs containing pheatmap() outputs are often broken (I
-        #       suspect it's something to do with not closing the PDF
-        #       graphics device, but I wasted too much time trying to fix
-        #       this).
-        filename = file.path(
-          outdir,
-          sprintf("%s.cell-level_heatmap.pdf", label)),
-        ...)
-    }
-  }
-}
-
-# Marker genes -----------------------------------------------------------------
-
-library(scuttle)
-library(scran)
-sce <- logNormCounts(sce)
-sce$tissue <- ifelse(grepl("Blood", sce$sample_name), "Blood", "Thymus")
-tissue_markers <- findMarkers(
-  sce,
-  sce$tissue,
-  direction = "up",
-  pval.type = "all")
-sample_name_markers <- findMarkers(
-  sce,
-  sce$sample_name,
-  direction = "up",
-  pval.type = "all")
-blood_sample_name_markers <- findMarkers(
-  sce[, sce$tissue == "Blood"],
-  sce$sample_name[sce$tissue == "Blood"],
-  direction = "up",
-  pval.type = "all")
-thymus_sample_name_markers <- findMarkers(
-  sce[, sce$tissue == "Thymus"],
-  sce$sample_name[sce$tissue == "Thymus"],
-  direction = "up",
-  pval.type = "all")
-
-outdir <- here("tmp")
-dir.create(file.path(outdir, "tissue"), recursive = TRUE)
-createClusterMarkerOutputs(sce, file.path(outdir, "tissue"), tissue_markers, k = 50)
-dir.create(file.path(outdir, "sample_name"), recursive = TRUE)
-createClusterMarkerOutputs(sce, file.path(outdir, "sample_name"), sample_name_markers, k = 50)
-dir.create(file.path(outdir, "blood"), recursive = TRUE)
-createClusterMarkerOutputs(sce, file.path(outdir, "blood"), blood_sample_name_markers, k = 50)
-dir.create(file.path(outdir, "thymus"), recursive = TRUE)
-createClusterMarkerOutputs(sce, file.path(outdir, "thymus"), thymus_sample_name_markers, k = 50)
-
-# DE of thymus3 and thymus1 ----------------------------------------------------
+#' # MDS plots
 
 library(edgeR)
 x <- SE2DGEList(sce)
-x <- x[, x$samples$sample_name %in% c("Thymus 3", "Thymus 1")]
-x$genes <- x$genes[, c("ENSEMBL.GENEID", "ENSEMBL.SYMBOL", "ENSEMBL.GENEBIOTYPE")]
-x$samples$group <- x$samples$sample_name
-table(x$samples$group)
 
-design <- model.matrix(~sample_name, x$samples)
+#' ## Individual technical replicates
+
+#+ fig.asp = 1, fig.cap = "Coloured by tissue"
+plotMDS(x, col = as.integer(factor(x$samples$tissue)))
+legend("bottomleft", legend = levels(factor(x$samples$tissue)), col = 1:nlevels(factor(x$samples$tissue)), pch = 16)
+#+ fig.asp = 1, fig.cap = "Coloured by sample_gate"
+plotMDS(x, col = as.integer(factor(x$samples$sample_gate)))
+legend("bottomleft", legend = levels(factor(x$samples$sample_gate)), col = 1:nlevels(factor(x$samples$sample_gate)), pch = 16)
+#+ fig.asp = 1, fig.cap = "Coloured by replicate"
+plotMDS(x, col = as.integer(factor(x$samples$replicate)))
+legend("bottomleft", legend = levels(factor(x$samples$replicate)), col = 1:nlevels(factor(x$samples$replicate)), pch = 16)
+
+#' ## Aggregated technical replicates
+
+y <- sumTechReps(
+  x,
+  paste0(x$samples$tissue, ".", x$sample$sample_gate, ".", x$sample$replicate))
+#+ fig.asp = 1, fig.cap = "Coloured by tissue"
+plotMDS(y, col = as.integer(factor(y$samples$tissue)))
+legend("topright", legend = levels(factor(x$samples$tissue)), col = 1:nlevels(factor(x$samples$tissue)), pch = 16)
+#+ fig.asp = 1, fig.cap = "Coloured by sample_gate"
+plotMDS(y, col = as.integer(factor(y$samples$sample_gate)))
+legend("topright", legend = levels(factor(x$samples$sample_gate)), col = 1:nlevels(factor(x$samples$sample_gate)), pch = 16)
+#+ fig.asp = 1, fig.cap = "Coloured by replicate"
+plotMDS(y, col = as.integer(factor(y$samples$replicate)))
+legend("topright", legend = levels(factor(x$samples$replicate)), col = 1:nlevels(factor(x$samples$replicate)), pch = 16)
+
+#' # DE analyses
+
+#' ## Dan's marker proteins
+marker_proteins <- read.csv(
+  here("data/marker_proteins/C094_Pellicci.marker_proteins.csv"))
+knitr::kable(marker_proteins)
+
+#' ## DE between stages in thymus
+
+library(Glimma)
+
+x <- SE2DGEList(sce)
+x <- x[, x$samples$tissue == "Thymus"]
+x$genes <- x$genes[, c("ENSEMBL.GENEID", "ENSEMBL.SYMBOL", "ENSEMBL.GENEBIOTYPE")]
+x$samples$group <- x$samples$sample_gate
+x <- sumTechReps(
+  x,
+  paste0(x$samples$tissue, ".", x$sample$sample_gate, ".", x$sample$replicate))
+
+#+ fig.asp = 1, fig.cap = "Coloured by sample_gate"
+plotMDS(x, col = as.integer(factor(x$samples$sample_gate)))
+legend("bottomleft", legend = levels(factor(x$samples$sample_gate)), col = 1:nlevels(factor(x$samples$sample_gate)), pch = 16)
+
+design <- model.matrix(~0 + sample_gate + replicate, x$samples)
 # NOTE: Decreasing `min.count` in order to test a few more genes.
 keep <- filterByExpr(x, min.count = 2)
-table(keep)
 x <- x[keep, , keep.lib.sizes = FALSE]
 x <- calcNormFactors(x)
 x <- estimateDisp(x, design)
 fit <- glmQLFit(x, design)
-qlf <- glmQLFTest(fit, coef = 2)
-summary(decideTests(qlf))
+contr <- makeContrasts(
+  Thymus.P8_vs_Thymus.P6 = sample_gateP8 - sample_gateP6,
+  Thymus.P7_vs_Thymus.P6 = sample_gateP7 - sample_gateP6,
+  Thymus.P8_vs_Thymus.P7 = sample_gateP8 - sample_gateP7,
+  levels = design)
 
-fit <- glmFit(x, design)
-lrt <- glmLRT(fit, coef=2)
-summary(decideTests(lrt))
+idx <- ids2indices(list(marker_proteins = marker_proteins$Gene), rownames(x))
 
-library(Glimma)
-glMDPlot(
-  lrt,
-  counts = x$counts,
-  groups = x$samples$group,
-  status = decideTests(lrt),
-  transform = TRUE,
-  path = here("output"),
-  html = "thymus3_vs_thymus1",
-  launch = FALSE)
+for (j in colnames(contr)) {
+  qlf <- glmQLFTest(fit, contrast = contr[, j])
+  print(summary(decideTests(qlf)))
+  glMDPlot(
+    qlf,
+    counts = x$counts,
+    groups = x$samples$group,
+    status = decideTests(qlf),
+    transform = TRUE,
+    path = here("output"),
+    html = j,
+    main = j,
+    launch = FALSE)
+  print(camera(x, idx, design, contrast = contr[, j]))
+  barcodeplot(qlf$table$logFC, index = idx$marker_proteins, main = j)
+}
+
+#' ## DE between blood (P5) and thymus (P8)
+
+x <- SE2DGEList(sce)
+x <- x[, (x$samples$tissue == "Thymus" & x$samples$sample_gate == "P8") |
+         (x$samples$tissue == "Blood" & x$samples$sample_gate == "P5")]
+x$genes <- x$genes[, c("ENSEMBL.GENEID", "ENSEMBL.SYMBOL", "ENSEMBL.GENEBIOTYPE")]
+x$samples$group <- paste0(x$samples$tissue, ".", x$samples$sample_gate)
+x <- sumTechReps(
+  x,
+  paste0(x$samples$tissue, ".", x$sample$sample_gate, ".", x$sample$replicate))
+
+#+ fig.asp = 1, fig.cap = "Coloured by tissue"
+plotMDS(x, col = as.integer(factor(x$samples$tissue)))
+legend("topleft", legend = levels(factor(x$samples$tissue)), col = 1:nlevels(factor(x$samples$tissue)), pch = 16)
+#+ fig.asp = 1, fig.cap = "Coloured by sample_gate"
+plotMDS(x, col = as.integer(factor(x$samples$sample_gate)))
+legend("topleft", legend = levels(factor(x$samples$sample_gate)), col = 1:nlevels(factor(x$samples$sample_gate)), pch = 16)
+
+design <- model.matrix(~0 + group + replicate, x$samples)
+# NOTE: Decreasing `min.count` in order to test a few more genes.
+keep <- filterByExpr(x, min.count = 2)
+x <- x[keep, , keep.lib.sizes = FALSE]
+x <- calcNormFactors(x)
+x <- estimateDisp(x, design)
+fit <- glmQLFit(x, design)
+contr <- makeContrasts(
+  Thymus.P8_vs_Blood.P6 = groupThymus.P8 - groupBlood.P5,
+  levels = design)
+
+for (j in colnames(contr)) {
+  qlf <- glmQLFTest(fit, contrast = contr[, j])
+  print(summary(decideTests(qlf)))
+  glMDPlot(
+    qlf,
+    counts = x$counts,
+    groups = x$samples$group,
+    status = decideTests(qlf),
+    transform = TRUE,
+    path = here("output"),
+    html = j,
+    main = j,
+    launch = FALSE)
+  print(camera(x, idx, design, contrast = contr[, j]))
+  barcodeplot(qlf$table$logFC, index = idx$marker_proteins, main = j)
+}
